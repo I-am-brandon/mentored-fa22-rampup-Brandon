c48c47940fc5c8e2cd04beb47807cd0dc1d82463
Brandon Penilla <brandon.pen@berkeley.edu>
2022-09-20 16:17:25 -0700

    First rough draft of website

--- script.js
+++ script.js
@@ -1,11 +1,37 @@
 function myFunction() {
+  alert("Thifasd");
+}
+function myFunctionC1() {
+  alert("This my first charcoal");
+}
+function myFunctionC2() {
+  alert("ffdsfd");
+}
+function myFunctionG1() {
+  alert("dfsddf");
+}
+function myFunctionG2() {
+  alert("sdfsd");
+}
+function myFunctionP1() {
+  alert("sdfsd");
+}
+function myFunctionP2() {
+  alert("This is the correct way to do js ☺️ 😊🥰👉👈");
+}
+function myFunctionA1() {
+  alert("This is the correct way to do js ☺️ 😊🥰👉👈");
+}
+function myFunctionA2() {
   alert("This is the correct way to do js ☺️ 😊🥰👉👈");
 }
 
+
+
 function submit() {
-    username = document.getElementById('username');
+    Namehere = document.getElementById('username');
     email = document.getElementById('email');
-    password = document.getElementById('password');
-    alert('Username:', username.value, 'Email:', email.value, 'Password:', password.value);
+    desc = document.getElementById('desc');
+    alert('Name:', name.value, 'Email:', email.value, 'Request description:', desc.value);
     return false;
 }
